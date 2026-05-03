db49a573546ec7a55447b1b5edb66628de9190ac
Clement <fermaudclement@gmail.com>
2026-05-03 21:18:58 +0200

    feat(mevReceipt): exportJson + module barrel

---
+++ apps/frontend/apps/cowswap-frontend/src/modules/mevReceipt/index.ts
@@ -0,0 +1,3 @@
+export { buildReceipt } from './services/buildReceipt'
+export { exportJson } from './services/exportJson'
+export type { MevProofReceipt, PartnerFeeInfo, BuildReceiptInput } from './types'
